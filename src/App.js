d7cedffc9adaa7754b2525140fa295393cb2dbac
MOHUND <msaadalharby@stu.kau.edu.sa>
2024-11-30 13:19:41 +0300

    Set base href in index.html and refactor App component for routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import RecipeDetails from "./components/recipe-details";
 
 import "./App.css";
+
 function App() {
   return (
-    <BrowserRouter>
-      <div class="container">
+    <BrowserRouter basename="/LAB-9">
+      <div className="container">
         <Nav />
         <main>
-          <BrowserRouter basename="/LAB-9">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/recipe-details/:id" element={<RecipeDetails />} />
-            </Routes>
-          </BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/recipe-details/:id" element={<RecipeDetails />} />
+          </Routes>
         </main>
         <Footer />
       </div>
